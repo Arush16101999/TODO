26c0c785ce424ba2f32e56c6f600bfde65fc4b97
arushmano <61136045+Arush16101999@users.noreply.github.com>
2023-11-05 21:15:11 +0530

    utils added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello World</h1>
+      <p>React App</p>
+    </div>
+  );
+}
+
+export default App;
